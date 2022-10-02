798cec1bb73834582aedab199dcb299e46317329
Cibele Martins <Cibele.Martins@ibm.com>
2022-10-02 13:42:20 -0300

    📦 feat: Implement FormControl and Notes page

--- src/components/FormControl/FormControl.js
+++ src/components/FormControl/FormControl.js
@@ -1,7 +1,51 @@
+// libs and hooks
+import { useState } from 'react';
+
+// styles
 import styles from './FormControl.module.css';
 
-function FormControl ({className, children}) {
-    return <div className={`${styles.FormControl} || ${className}`}>{children}</div>
+// components
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+
+function FormControl ({className, children, renderChildren,}) {
+
+    // input title 
+     const [titleNote, setTitleNote] = useState('')
+    const changeTitleNote = (event)=> setTitleNote(event.target.value) 
+    
+    // input description
+    const [description, setDescription] = useState('')
+    const changeDescription = (event)=> setDescription(event.target.value) 
+
+    // contents
+    const arrayChildrens = [];
+    const arrayInputs = [];
+
+    // conditional for render contents
+    if (renderChildren) {
+
+     arrayChildrens.push(children)
+    } else {
+
+        arrayInputs.push(<>  <Input
+            placeholder="Title of your note"
+            onChange={(event)=> changeTitleNote(event)}
+            value={titleNote}/>
+            <textarea
+            className={styles.description}
+            value={description}
+            onChange={(event)=> changeDescription(event)}
+            placeholder="Describe your note"></textarea>
+            <Button title="Insert"/>
+        </>)
+    }
+
+    // component
+    return <div className={`${styles.FormControl} || ${className}`}>
+            {arrayChildrens}
+            {arrayInputs}
+    </div>
 }
 
 export default FormControl;
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -8,18 +8,8 @@ import styles from './Notes.module.css';
 import FormContainer from '../components/FormContainer/FormContainer';
 import Title from '../components/Title/Title';
 import FormControl from '../components/FormControl/FormControl';
-import Input from '../components/Input/Input';
-import Button from '../components/Button/Button';
 
 function Notes () {
-
-    // input title 
-    const [titleNote, setTitleNote] = useState('')
-    const changeTitleNote = (event)=> setTitleNote(event.target.value) 
-
-    // input description
-    const [description, setDescription] = useState('')
-    const changeDescription = (event)=> setDescription(event.target.value) 
 
     // informations of post it
     const [postIt, setPostIt] = useState([]);
@@ -38,16 +28,6 @@ function Notes () {
                 <Title 
                 text="Write here all your tasks, goals, concerns and anything else you want! "/>
                 <FormControl>
-                    <Input
-                    placeholder="Title of your note"
-                    onChange={(event)=> changeTitleNote(event)}
-                    value={titleNote}/>
-                    <textarea
-                    className={styles.description}
-                    value={description}
-                    onChange={(event)=> changeDescription(event)}
-                    placeholder="Describe your note"></textarea>
-                    <Button title="Insert"/>
                 </FormControl>
             </FormContainer>
         </>
